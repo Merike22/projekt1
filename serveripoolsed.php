a2cc45f69370c44b2d213055ec4ba464585a5bc1
Merike Tomson <merike.tomson@khk.ee>
2015-09-15 14:45:30 +0300

    Refaktoorida skript selliselt, et ees-, perenimi ja vanus asuvad $isik nimelises massiivis liikmetena, mille võtmed on vastavalt eesnimi ja perenimi ja vanus. Kuvada sama teksti (ehk nime koos vanusega sulgudes) välja nüüd sellest massiivist. [Delivers #103399618]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,3 +5,5 @@ $Perenimi = 'Tomson';
 $Vanus = 22;
 echo "$Eesnimi $Perenimi ($Vanus)";
 $Eesnimi_algab_vokaaliga=false;
+$isik = ['eesnimi'=>$Eesnimi, 'perenimi'=>$Perenimi, 'vanus'=>$Vanus];
+echo "{$isik['eesnimi']} {$isik ['perenimi']} ({$isik ['vanus']})";
