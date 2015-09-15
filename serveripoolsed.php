dde050af3e5da60d12a1b6d19ae85ad69fb521f0
Merike Tomson <merike.tomson@khk.ee>
2015-09-15 15:28:14 +0300

    Refaktoorida massiiv objektiks, milles on lisaks massiivis olnud liikmetele ka atribuut (ehk omadus) sugu, väärtusega vastavalt kas mees või naine. Instantseerida see objekt ja kuvada selle objekti omadusi esialgses echo reas, mis kuvas nime välja, asendades seal olnud massiivi [Delivers #103402118]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,10 +5,11 @@ $Perenimi = 'Tomson';
 $Vanus = 22;
 echo "$Eesnimi $Perenimi ($Vanus)";
 $Eesnimi_algab_vokaaliga=false;
-$isik = ['eesnimi'=>$Eesnimi, 'perenimi'=>$Perenimi, 'vanus'=>$Vanus];
-echo "{$isik['eesnimi']} {$isik ['perenimi']} ({$isik ['vanus']})";
+$isik = (object)['eesnimi'=>$Eesnimi, 'perenimi'=>$Perenimi, 'vanus'=>$Vanus];
+$isik->sugu='Naine';
+echo "{$isik->eesnimi} {$isik->perenimi} ({$isik->vanus}) {$isik->sugu}";
 if (in_array(strtolower($Eesnimi[0]), ['a','e','i','o','u','õ','ö','ö','ü'])) {
-    echo "Nimi algab vokaaliga";
+    echo " Nimi algab vokaaliga";
 } else {
-    echo "Nimi ei alga vokaaliga";
+    echo " Nimi ei alga vokaaliga";
 }
