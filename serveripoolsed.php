4c38a2c5c474018cdb27101b9a3aeb0a2e0a5623
Merike Tomson <merike.tomson@khk.ee>
2015-09-15 15:09:37 +0300

    Lisada lehele if tingimus, mis kuvab ekraanil "Nimi algab vokaaliga", kui nimi algab vokaaliga [Delivers #103401022]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,3 +7,8 @@ echo "$Eesnimi $Perenimi ($Vanus)";
 $Eesnimi_algab_vokaaliga=false;
 $isik = ['eesnimi'=>$Eesnimi, 'perenimi'=>$Perenimi, 'vanus'=>$Vanus];
 echo "{$isik['eesnimi']} {$isik ['perenimi']} ({$isik ['vanus']})";
+if (in_array(strtolower($Eesnimi[0]), ['a','e','i','o','u','õ','ö','ö','ü'])) {
+    echo "Nimi algab vokaaliga";
+} else {
+    echo "Nimi ei alga vokaaliga";
+}
