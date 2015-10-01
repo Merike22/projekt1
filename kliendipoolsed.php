8892bf5fd4010226dc931b0d5f18dacb94c26117
Merike Tomson <merike.tomson@khk.ee>
2015-10-01 12:28:07 +0300

    Lisa dokumenti pilt ilusast kassist ja tee nii, et kui pilti klikitakse, siis muutub kassipilt koerapildiks [Delivers #104611086]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -18,10 +18,22 @@
 <a href="http://localhost/projekt1/kliendipoolsed.php?">
     <span>j‰‰me siia</span>
 </a>
+<a href='#' onclick='edit()'><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRcwlRKAlSIaCI4W5PRYVbuBQQXifF-56bFqAjh9DMe-_3Lh8_YKw" id="koer_kass"/></a>
 
 
-<?php
-?>
+<script>
+function edit()
+{
+    var inputs = document.myform;
+    for(var i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = false;
+    }
+}
+var koer_kass = document.getElementById("koer_kass");
+koer_kass.onclick = function(){
+    this.src = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRDcZIsFoJJMGaMbu_wLwmGvVzh_waZrNzMrWV6ZsNowj6YMKbh";
+}
+</script>
 </body>
 </html>
 
