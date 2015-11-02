b3c80a6792d58894799f217a4858d54ea10fcd5d
Merike Tomson <merike.tomson@khk.ee>
2015-11-02 21:26:40 +0200

    Lisada eelmises ülesandes loodud faili HTML osa ette PHP osa, kus luuakse $page nimeline muutuja ternary if-i abil, mille väärtus, kui $_GET['page'] eksisteerib, on see, mis on $_GET['page'] väärtus, ja kui $_GET['page'] muutuja ei eksisteeri, siis on väärtuseks string 'home'. Refaktoorida <body> osas olnud PHP osa, eemaldades sealt muutuja eksisteerimise kontrolli ja requireides $page muutuja väärtuse nimelise faili (pluss .php lõppu).. [Delivers #105981424]

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 </head>
 <body>
 <?php
+$var = 5;
+$var_is_greater_than_two = ($var > 2 ? true : false); // returns true
+?>
+<?php
    if(isset($_GET['page'])){
        require $_GET['page'].".php";
    }
