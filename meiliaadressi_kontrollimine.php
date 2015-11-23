e18d14e148644e1fa9b4cedae08b90ee2e3ab20e
Merike Tomson <merike.tomson@khk.ee>
2015-11-23 13:12:17 +0200

    Loo fail meiliaadressi_kontrollimine.php, kuhu tee tekstiväli, kuhu saab sisestada meiliaadressi, ja nupp. Nupule vajutades tuleb teade meiliaadress kehtib, kui meiliaadress kehtib, meiliaadress ei vasta õigele formaadile, kui meiliaadress ei vasta õigele formaadile. Tee kontrollid javascriptiga. [Delivers #108744944]

---
+++ meiliaadressi_kontrollimine.php
@@ -0,0 +1,18 @@
+<input type="text" id="meil" placeholder="sinumeil@veebiaadress.lühend" size="25"
+       onkeypress="Javascript: if (event.keyCode==13) kontrolliEmaili();"/>
+<input type="submit" value="Kontrolli" name="kontrolli" onclick="Javascript:kontrolliEmaili();"/>
+<script type="text/javascript">
+    function kontrolliEmaili() {
+        var meil = document.getElementById('meil');
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!filter.test(meil.value)) {
+            alert('Meiliaadress ei vasta õigele formaadile!');
+            meil.focus;
+            return false;
+        }
+        else {
+            alert('Meiliaadress kehtib!');
+            return true;
+        }
+    }
+</script>
